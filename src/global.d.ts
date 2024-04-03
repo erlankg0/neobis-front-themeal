8aafec90a4540d932b8b3160e42d61da152ca4b5
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-04-04 00:17:32 +0300

    deploy

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.module.css' {
-    const styles: { [key: string]: string };
-    export default styles;
-}
-
-
-
-
--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './layout/App/App.tsx'
-import './index.css'
-import './normalize.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App/>
-)
